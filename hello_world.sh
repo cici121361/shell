623f686a309e1aa80fc422f7b2576bafe6f881a0
cici121361 <77155501+cici121361@users.noreply.github.com>
2021-08-04 15:50:02 +0800

    Install2_3

--- hello_world.sh
+++ hello_world.sh
@@ -1,7 +1,18 @@
-echo "Step1 Update APT"
-sudo apt update
-
 echo "Step2 Install Python"
-sudo apt install python3-pip python3-dev
+sudo apt install software-properties-common
+sudo add-apt-repository ppa:deadsnakes/ppa
 sudo apt install python3.9
+
+echo "Also, install setuptools to config python"
+sudo pip install --upgrade pip
 pip install setuptools>=40.0.0
+
+sudo in -s /usr/bin/python3.9 /usr/bin/python
+sudo in -s /usr/bin/python3.9 /usr/bin/python
+
+#check out python version
+echo "python version:"
+python --version
+echo "python3 version"
+python3 --version
+
