ae561852a0761f5ef433bc9b6a8bcb5179c4e0ef
cici121361 <77155501+cici121361@users.noreply.github.com>
2021-08-04 15:29:31 +0800

    install part1
    install python3.9

--- hello_world.sh
+++ hello_world.sh
@@ -1,1 +1,7 @@
-echo "hello,world!"
+echo "Step1 Update APT"
+sudo apt update
+
+echo "Step2 Install Python"
+sudo apt install python3-pip python3-dev
+sudo apt install python3.9
+pip install setuptools>=40.0.0
