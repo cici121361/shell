a0b9db4896476e29b101e642d92b14f09e4356ce
cici121361 <77155501+cici121361@users.noreply.github.com>
2021-08-05 15:24:16 +0800

    Update install_part2.sh

--- install_part2.sh
+++ install_part2.sh
@@ -1,8 +1,3 @@
-config_location  = /root/.jupyter/jupyter_lab_config.py
-echo c.ServerApp.ip='*' >> config_location
-echo c.NotebookApp.open_browser = False >> config_location
-echo c.ServerApp.port =8890 >> config_location
-
 echo "#******************Part2:Install_nginx********************#"
 # sudo apt install software-properties-common  #already install
 sudo add-apt-repository ppa:certbot/certbot
