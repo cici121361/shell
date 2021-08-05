e1d54f3e888acc8e97568f44df6d2f4d64fb86c9
cici121361 <77155501+cici121361@users.noreply.github.com>
2021-08-05 15:22:57 +0800

    Update install_part1.sh

--- install_part1.sh
+++ install_part1.sh
@@ -1,20 +1,32 @@
 # Automatic Installation
 echo "Welcome to use Cici's Automatic Install Script! It is easy to configure the JupyterLab environment by this way."
+echo "Please make sure you enter this shell in the bottom path"
 
-echo "Step1 Update APT"
-sudo apt update
+echo "#******************Part1:InstallPython********************#"
+echo "In the part, we will install python and configure jupyter lab in the virtual environment. "
+echo "###########Step1 Update APT###########"
+sudo apt update && sudo apt full-upgrade
 
-echo "Step2 Install Python"
+echo "###########Step2 Install Python###########"
+echo "+++++++++++++++++++++++++"
+echo "install software-properties-common"
 sudo apt install software-properties-common
+
+echo "+++++++++++++++++++++++++"
+echo "Add-apt-repository ppa:deadsnakes/ppa"
 sudo add-apt-repository ppa:deadsnakes/ppa
+
+echo "+++++++++++++++++++++++++"
+echo "install python3.9"
 sudo apt install python3.9
 
+echo "+++++++++++++++++++++++++"
 echo "Also, install setuptools to config python"
 sudo apt install python3-pip
 pip3 install setuptools>=40.0.0
 
-sudo in -s /usr/bin/python3.9 /usr/bin/python
-sudo in -s /usr/bin/python3.9 /usr/bin/python
+sudo ln -s /usr/bin/python3.9 /usr/bin/python
+sudo ln -s /usr/bin/python3.9 /usr/bin/python3
 
 #check out python version
 echo "python version:"
@@ -22,34 +34,59 @@ python --version
 echo "python3 version"
 python3 --version
 
-echo "Step3 Install Python virtualenv"
+echo "###########Step3 Create Jupyter Lab virtualenv###########"
+echo "install venv"
 pip3 install virtualenv
 
-echo "Step4 Create Python Virtual Evironment"
-mkdir env
-cd env
-virtualenv jupyterlab       #creat virtual environment
-source env/jupyterlab/bin/activate
+echo "+++++++++++++++++++++++++"
+echo"load venv"
+echo "input filename to save virtual environment"
+read filename
 
-echo "Step5 Install Jupyter Lab in virtual environment"
-pip install jupyter lab
+mkdir $filename
+cd $filename
+echo "entitle the name of this virtual environment to save the virtual environment"
+read envname
+virtualenv $envname       #creat virtual environment
+cd $envname
+source bin/activate
+
+echo "+++++++++++++++++++++++++"
+echo "Install Jupyter Lab in virtual environment"
+pip3 install jupyterlab -i https://pypi.tuna.tsinghua.edu.cn/simple/ #use the mirror provided by Tsinghua University
 jupyter lab --generate-config
 
 #set a password
-echo "Step6 Set a password to enter jupyter lab"
-jupyter lab password
+echo "###########Step4 Configure jupyter lab###########"
+# echo "set a password to enter jupyter lab in the website"
+# apt install ipython
+# ipython
+# from notebook.auth import passwd
+# passwd()
 
-#revise config file
+# exit()
+
+# revise config file
+# echo "the location of configuration file path:"
+# whereis jupyter_lab_config
 config_location  = /root/.jupyter/jupyter_lab_config.py
+# echo "check out the location of configuration file is $config_location ?Y/n"
+# read judge
+# case $judge in
+#     Y|y) pass;;
+#     N|n)echo "input the correct location:"
+#            read config_location;;
+# esac
 
 #add the needing content into config file
-echo c.ServerApp.ip='*' >> config_location
-echo c.NotebookApp.open_browser = False >> config_location
-echo c.ServerApp.port =8890 >> config_location
+# echo "add the necessary information into configuration file"
+# echo c.ServerApp.ip='*' >> config_location
+# echo c.NotebookApp.open_browser = False >> config_location
+# echo c.ServerApp.port =8890 >> config_location
 
 #test jupyterlab
-jupyter lab
+#echo "test whether or not enter jupyter lab fluently"
+#jupyter lab
 
 #shut down virtual environment
-echo "shut down the virtual environment"
-deactivate
+#echo "if you finish the test , Please input [deactivate] to shut down the virtual environment"
