a4bae805350aa78e7e99b8875f9e29a31efdc97d
cici121361 <77155501+cici121361@users.noreply.github.com>
2021-08-05 10:27:01 +0800

    Add files via upload

---
+++ install_part1_test3.sh
@@ -0,0 +1,24 @@
+#revise config file
+echo "the location of configuration file path:"
+whereis jupyter_lab_config
+config_location  = /root/.jupyter/jupyter_lab_config.py
+echo "check out the location of configuration file is $config_location ?Y/n"
+read judge
+case $judge in
+     Y|y) pass;;
+     N|n)echo "input the correct location:"
+            read config_location;;
+esac
+
+#add the needing content into config file
+echo "add the necessary information into configuration file"
+echo c.ServerApp.ip='*' >> config_location
+echo c.NotebookApp.open_browser = False >> config_location
+echo c.ServerApp.port =8890 >> config_location
+
+#test jupyterlab
+echo "test whether or not enter jupyter lab fluently"
+jupyter lab
+
+#shut down virtual environment
+echo "if you finish the test , Please input [deactivate] to shut down the virtual environment"
