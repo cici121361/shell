ffa833e0cc095f7502e8f33e810b4e30a03426f5
cici121361 <77155501+cici121361@users.noreply.github.com>
2021-08-05 10:31:02 +0800

    Update install_part1_test3.sh

--- install_part1_test3.sh
+++ install_part1_test3.sh
@@ -5,13 +5,16 @@ config_location  = /root/.jupyter/jupyter_lab_config.py
 echo "check out the location of configuration file is $config_location ?Y/n"
 read judge
 case $judge in
-     Y|y) pass;;
+     Y|y) echo "continue to enter the later step";;
      N|n)echo "input the correct location:"
             read config_location;;
 esac
 
 #add the needing content into config file
 echo "add the necessary information into configuration file"
+echo "input the str which represents the password"
+read password
+echo c.ServerAPP.password = u$password >> config_location
 echo c.ServerApp.ip='*' >> config_location
 echo c.NotebookApp.open_browser = False >> config_location
 echo c.ServerApp.port =8890 >> config_location
