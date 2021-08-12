a8fda708fbb93c922ec7d130633a37f7a2963053
cici121361 <77155501+cici121361@users.noreply.github.com>
2021-08-12 11:57:48 +0800

    Update install_jupterlab2.sh
    第二版

--- install_jupterlab2.sh
+++ install_jupterlab2.sh
@@ -11,40 +11,61 @@ prefix=$HOME/jupyterlab
 #check: the version of python
 pyv="$(python -V 2>&1)"
 py3v="$(python3 -V 2>&1)"
-case $pyv in
+case "$(python -V 2>&1)" in
     "python 3.9"|"python 3.8")echo "the python version is $pyv";;
     *)echo "the python version is $pyv, check out existed python version and update the version if counldn't find."
     if [-z $(dpkg -l | grep python3.9)] || [-z $(dpkg -l | grep python3.8)];then
-        sudo pip install python3.9
+        sudo apt update
+        sudo apt install software-properties-common
+        sudo add-apt-repository ppa:deadsnakes/ppa
+        sudo pip install python3.8
+        ln -s /usr/bin/python3.8 /usr/bin/python
+        if ["$(pip --version)"=~"not found"];then sudo apt install python-pip; fi
+    elif [-n $(dpkg -l | grep python3.8)];then
+        ln -s /usr/bin/python3.8 /usr/bin/python
+    else
         ln -s /usr/bin/python3.9 /usr/bin/python
-        sudo
-        
-        
+        if ["$(pip --version)"=~"not found"];then sudo apt install python-pip; fi
+        pip3 install setuptools>=40.0.0
+    fi
     ;;
 esac
 
+#install jupyterlab
+if ["$(jupyter lab --version)"=~"not found"];then pip install jupyterlab; fi
+echo "set a password to enter jupyter lab in the website"
+apt install ipython
+ipython << cici
+from notebook.auth import passwd
+passwd() #a=passwd(),but variables a cannot use in global environment.
+quit()
+cici
 
-#connection: python/python3 represent which version of python
-#install or not: python3.9 / miniconda / nginx
+echo "input the str which jupyter output"
+read -r passstr
+while true; do
+    case "$passstr" in
+        
+    
 
+config_location=/root/.jupyter/jupyter_lab_config.py
+config_inf="c.ServerApp.ip='*'\n c.NotebookApp.open_browser = False \n c.ServerApp.port =8890 \n c.ServerApp.password =$passstr"
+echo "add the necessary information into configuration file"
+echo $config_inf >> $config_location
+
+#whether or not install the virtual environment: virualenv / miniconda
+#set a password
 
 
 
 # deploy nginx
-
-nginx_version="1.18.0"
-openssl_version="1.1.1k"
-zlib_version="1.2.11"
-pcre_version="8.44"
-
 nginx_path="/usr/local/nginx"
 
-function checkFile(){
-    if [! -f "$1"];then
-        echo "$1 is not exited"
-        exit 1
-    fi
-}
+if ["$(nginx -V)"=~"not found"];then sudo apt install nginx; fi
+sudo add-apt-repository ppa:certbot/certbot
+sudo apt install python-certbot-nginx
+echo "install certbot"
+sudo certbot --nginx
 
 
 
